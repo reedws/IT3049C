e8b220b0e45ad0d93fbdbfc65cb4be3c6a977f70
Yahya Gilany <yahya.gilany@live.com>
2020-02-22 21:26:36 -0500

    new labs

---
+++ Labs/3.Web_Storage/start/main.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const nameInput = document.getElementById("myName");
+const myMessage = document.getElementById("myMessage");
+const sendButton = document.getElementById("sendButton");
+const chatBox = document.getElementById("chat");
+
+const serverURL = `http://ed3d11da.ngrok.io/messages`;
+
+function fetchMessages() {
+    return fetch(serverURL)
+        .then( response => response.json())
+}
+
+function formatMessage(message, myName) {
+    const time = new Date(message.timestamp);
+    const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+    if (myName === message.sender) {
+        return `
+        <div class="mine messages">
+            <div class="message">
+                ${message.text}
+            </div>
+            <div class="sender-info">
+                ${formattedTime}
+            </div>
+        </div>
+        `
+    } else {
+        return `
+            <div class="yours messages">
+                <div class="message">
+                    ${message.text}
+                </div>
+                <div class="sender-info">
+                    ${message.sender} ${formattedTime}
+                </div>
+            </div>
+        `
+    }
+}
+
+async function updateMessages() {
+    // Fetch Messages
+    const messages = await fetchMessages();
+    // Loop over the messages. Inside the loop we will:
+        // get each message
+        // format it
+        // add it to the chatbox
+    let formattedMessages = "";
+    messages.forEach(message => {
+        formattedMessages += formatMessage(message, nameInput.value);
+    });
+    chatBox.innerHTML = formattedMessages;
+}
+
+setInterval(updateMessages, 2000);
+
+
+function sendMessages () {
+    const newMessage = {
+        sender: nameInput.value,
+        text: myMessage.value,
+        timestamp: new Date()
+    }
+
+    $.post(serverURL, newMessage);
+}
+
+sendButton.addEventListener("click", function(sendButtonClickEvent) {
+    sendButtonClickEvent.preventDefault();
+
+    sendMessages();
+    myMessage.value = "";
+});
